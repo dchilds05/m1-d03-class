fdc67f2f271c642620b26ef9b26d4a6b15d79652
marco-ih-bcn <marco.santonastasi@ironhack.com>
2021-06-02 15:41:15 +0200

    WIP step 2

--- 02_code-js_objects.js
+++ 02_code-js_objects.js
@@ -2,10 +2,26 @@
  * Code along objects
  */
 
-// The reason for objects
+// Objects as a protection for naming
+const order1 = {
+  beverage: "cappucino",
+  price: 10,
+  sugar: false,
+  extraFoam: true,
+};
+
+const order2 = {
+  beverage: "cappucino",
+  price: 10,
+  sugar: false,
+  extraFoam: true,
+};
+
+/* // The reason for objects
 let beverage = "cappucino";
 const price = 10;
 const sugar = false;
 const extraFoam = true;
 
 beverage = "caffé"; // this is not possible because it will overwrite the first one
+ */
